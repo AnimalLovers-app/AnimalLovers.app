e24790e1bfcf14e5dd2f59e71a87316780fe2b21
hmis55 <158746092+hmis55@users.noreply.github.com>
2024-03-18 05:38:18 +0000

    [fix] appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,3 @@
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource)
-    about_path
-  end
-
-  def after_sign_out_path_for(resource)
-    about_path
-  end
-
-  def after_sign_in_path_for(resource)
-    case resource
-    when Admin
-      admin_homes_top_path
-    when Customer
-      admin_homes_top_path
-    else
-      admin_homes_top_path
-    end
-  end
+ 
 end
