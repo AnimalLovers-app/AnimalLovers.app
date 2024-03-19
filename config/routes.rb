a73fe21036e5da0f73bb26b25a4b5bb1d8f1afe1
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-03-19 07:52:31 +0000

    [Add]controller,view,routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,24 @@
 Rails.application.routes.draw do
 
+# 顧客側のルーティング設定
+  scope module: :public do
+    root 'homes#top'
+    get 'about' => 'homes#about'
+  end
+
+# 管理者側のルーティング設定
   namespace :admin do
     get 'homes/top'
   end
-  
-  
+
+# deviseのルーティング設定
   devise_for :admin, skip: [:passwords] ,controllers: {
   sessions: "admin/sessions"
   }
 
-  # devise_for :customers
- devise_for :customers,skip: [:passwords], controllers: {
+  devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: "public/sessions"
-}
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  }
+
 end
