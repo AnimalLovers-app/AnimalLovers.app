a047388956589875a1f74c75702759c411a01da8
EC2 Default User <ec2-user@ip-172-31-0-218.ap-northeast-1.compute.internal>
2024-03-19 07:34:52 +0000

    [Add]編集・削除ボタンの追加

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -17,10 +17,25 @@ class AddressesController < ApplicationController
   end
 
   def edit
-    @address = Address.get(params[:id])
+    @address = Address.find(params[:id])
+    unless @address.customer_id == current_customer.id
+      redirect_to addresses_path
+    end
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :addresses, only: [:index, :edit, :create, :update, :destrory]
+  resources :addresses, only: [:index, :edit, :create, :update, :destroy]
 end
