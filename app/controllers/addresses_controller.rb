13dfbcb5f601f225fc5c127e06337440600297d0
EC2 Default User <ec2-user@ip-172-31-0-218.ap-northeast-1.compute.internal>
2024-03-19 05:53:33 +0000

    [Add]ビューに登録フォームと配送先一覧追加

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,22 +1,33 @@
 class AddressesController < ApplicationController
 
   def index
-    @addresses = Address.find_by(customer_id: current_customer)
-    @newAddress = Address.new
+    @addresses = current_customer.addresses
+    @address = Address.new
   end
 
   def create
     @address = Address.new(address_params)
-    @address.customer_id = current_user.id
+    @address.customer_id = current_customer.id
     if @address.save
       redirect_to addresses_path
     else
       @addresses = Address.find_by(customer_id: current_customer)
-      render "index"
+      render :index
     end
   end
 
   def edit
     @address = Address.get(params[:id])
   end
+
+  def destroy
+  end
+
+
+  private
+
+  def address_params
+  params.require(:address).permit(:postal_code, :address, :name)
+  end
+
 end
