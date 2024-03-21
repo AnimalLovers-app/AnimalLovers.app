2b211502ff0693974a8f5901a04b6e56d9ea3492
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-03-21 14:17:41 +0000

    [Add]header,footer

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -25,11 +25,10 @@ class Public::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
   def after_sign_in_path_for(resource)
-    # 遷移先のパス
-      admin_homes_top_path
+      admin_root_path
   end
-  
+
   def after_sign_out_path_for(resource)
-      admin_homes_top_path
+      admin_root_path
   end
 end
