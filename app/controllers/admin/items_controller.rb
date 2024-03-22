7ba4fa683ede8601976c2251ddff267b14ba1f4c
EC2 Default User <ec2-user@ip-172-31-9-69.ap-northeast-1.compute.internal>
2024-03-22 05:38:24 +0000

    [Add]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -15,8 +15,8 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
+    item = Item.new(item_params)
+    if item.save
       redirect_to admin_items_path
     else
       render :new
--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,31 +5,31 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = current_customer.cart_items.build(cart_item_params)
-    @cart_items = current_customer.cart_items.all
-    @cart_items.each do |cart_item|
-      if cart_item.item_id == @cart_item.item_id
-        new_amount = cart_item.amount + @cart_item.amount
+    cart_item = current_customer.cart_items.build(cart_item_params)
+    cart_items = current_customer.cart_items.all
+    cart_items.each do |cart_item|
+      if cart_item.item_id == cart_item.item_id
+        new_amount = cart_item.amount + cart_item.amount
         cart_item.update_attribute(:amount, new_amount)
-        @cart_item.delete
+        cart_item.delete
       end
     end
-    @cart_item.save
+    cart_item.save
     redirect_to cart_items_path
   end
-  
+
   def update
-    @cart_item = CartItem.find(params[:id])
-    if @cart_item.update(cart_item_params)
+    cart_item = CartItem.find(params[:id])
+    if cart_item.update(cart_item_params)
       redirect_to cart_items_path, notice: 'カート内の商品の数量を更新しました'
     else
       render :index
     end
   end
-  
+
   def destroy
-    @cart_item = CartItem.find(params[:id])
-    @cart_item.destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
     redirect_to cart_items_path, notice: 'カート内の商品を削除しました。'
   end
 
