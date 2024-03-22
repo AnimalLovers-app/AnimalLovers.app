cabc153ad6f5c36908eff07dd53ab1146f94015b
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-03-22 08:02:54 +0000

    [Add]コントローラ、View

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,12 +4,53 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @order = Order.new(order_params)
+    @cart_items = current_member.cart_items.all
+    @total = 0
+
+    # [:address_option]=="0"の場合、current_customerの住所を呼び出す
+    if params[:order][:address_option] == "0"
+      @order.post_code = current_customer.post_code
+      @order.address = current_customer.address
+      @order.name = current_customer.family_name + current_customer.first_name
+
+    # [:address_option]=="1"の場合、プルダウンで選択された値から呼び出す
+    elsif params[:order][:address_option] == "1"
+      ship = Address.find(params[:order][:customer_id])
+      @order.post_code = ship.post_code
+      @order.address = ship.address
+      @order.name = ship.name
+
+    # [:address_option]=="2"の場合、formに入力されたデータを受け取る
+    elsif params[:order][:address_option] = "2"
+      @order.post_code = params[:order][:post_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+
+    else
+      render 'new'
+    end
   end
 
   def thanks
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.save
+
+    current_customer.cart_items.each do |cart_item|
+      @ordered_item = OrderedItem.new
+      @ordered_item.order_id =  @order.id
+      @ordered_item.item_id = cart_item.item_id
+      @ordered_item.amount = cart_item.amount
+      @ordered_item.price = cart_item.subtotal
+      @ordered_item.save
+    end
+
+    current_member.cart_items.destroy_all
+    redirect_to orders_thanks_path
   end
 
   def index
@@ -17,4 +58,10 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+
+  private
+    def order_params
+        params.require(:order).permit(:customer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status)
+    end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
   has_many :order_details
+  has_many :items, through: :order_details
   belongs_to :customer, optional:true
 end
