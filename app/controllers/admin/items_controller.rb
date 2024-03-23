29e276329e2df2d99ae7b4da4d91a73ba5035978
EC2 Default User <ec2-user@ip-172-31-9-69.ap-northeast-1.compute.internal>
2024-03-23 12:20:54 +0000

    [Add] bootstrap

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,15 +1,16 @@
 class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def new
     @item = Item.new
   end
 
   def index
-    @items = Item.all
+    @items = Item.page(params[:page]).per(10)
   end
 
   def show
+    @items = Item.all
     @item = Item.find(params[:id])
   end
 
@@ -25,9 +26,14 @@ class Admin::ItemsController < ApplicationController
       render :new
     end
   end
-  
+
   def update
-    
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to admin_item_path(item.id)
+    else
+      render 'edit'
+    end
   end
 
   private
