ca21fcbf656ec8854ec0e2905997ee8882fbfe3b
EC2 Default User <ec2-user@ip-172-31-0-218.ap-northeast-1.compute.internal>
2024-03-24 14:34:38 +0000

    [Fix]不要な記述を削除2

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,7 +2,6 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
 
   def top
-    @customers = Customer.all
     @orders = Order.page(params[:page]).per(10)
   end
 end
