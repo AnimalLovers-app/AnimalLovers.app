8d6cbe5acff7481e802fdd5d12919bd9b71cd5b2
EC2 Default User <ec2-user@ip-172-31-0-218.ap-northeast-1.compute.internal>
2024-03-24 04:44:30 +0000

    [Fix]顧客側/itemコントローラのshowアクション

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -6,8 +6,6 @@ class Public::ItemsController < ApplicationController
   end
 
   def show
-    @item_all = Item.all
-    @items = Item.page(params[:page]).per(8)
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
     @genres = Genre.all
