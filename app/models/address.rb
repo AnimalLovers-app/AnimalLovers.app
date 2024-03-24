6afe12ebc8fe6bceb03eb81accc6970c3712c919
EC2 Default User <ec2-user@ip-172-31-0-218.ap-northeast-1.compute.internal>
2024-03-24 07:37:23 +0000

    [Fix]コンフリクト修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,14 +2,11 @@ class Address < ApplicationRecord
 
   belongs_to :customer
 
-<<<<<<< HEAD
-  validates :postal_code, presence: true
-  validates :address, presence: true
-  validates :name, presence: true
-=======
   def address_display
     '〒' + postal_code + ' ' + address + ' ' + name
   end
->>>>>>> origin/develop
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
 
 end
