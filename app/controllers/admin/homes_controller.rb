5dd5293f4b1ce958ee54e728a0cf2d4ffb678369
EC2 Default User <ec2-user@ip-172-31-0-218.ap-northeast-1.compute.internal>
2024-03-24 13:57:22 +0000

    [Add]管理者トップページ

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,9 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def top
+    @customers = Customer.all
+    @orders = Order.page(params[:page]).per(10)
+    @total_amount = 0
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,11 @@ class Order < ApplicationRecord
   has_many :order_details
   has_many :items, through: :order_details
   belongs_to :customer, optional:true
-  
+
   enum payment_method: { credit_card: 0, transfer: 1 }
+
+  #注文履歴一覧でamountのトータルを計算するメソッド
+  def sum_of_amount
+    order_details.sum(:amount)
+  end
 end
