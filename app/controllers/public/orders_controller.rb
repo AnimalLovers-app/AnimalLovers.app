62eb29aa9f86eb72170f0b4ef65f4b7b2b24c85d
EC2 Default User <ec2-user@ip-172-31-9-69.ap-northeast-1.compute.internal>
2024-03-25 05:14:06 +0000

    [Add] index,show

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -60,11 +60,14 @@ class Public::OrdersController < ApplicationController
 
   def index
     @orders = Order.all
+    @order_details = @order.order_details.all
+    @total=0
   end
 
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details.all
+    @total=0
   end
 
   private
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,8 @@ class Order < ApplicationRecord
   belongs_to :customer, optional:true
   
   enum payment_method: { credit_card: 0, transfer: 1 }
+  
+  def subtotal
+    item.with_tax_price * amount
+  end
 end
